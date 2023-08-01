85cd3ece93b9b68ffdb30c0d388372cdef6f3712
Siva_Sai <22je0483@iitism.ac.in>
2023-08-01 19:49:58 +0530

    Add the hover effect to change the color of grid

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const body = document.querySelector('body');
-const main_div = document.createElement('div');
+const main_div = document.querySelector('.main_div');
+
 for(let i=0;i<16;i++){
     const column_div = document.createElement('div');
+    column_div.classList.add('one-row');
     for(let j=0;j<16;j++){
         const row_div = document.createElement('div');
-        row_div.style.cssText = 'width: 25px; height: 25px;'
-        column_div.appendChild(row_div)
+        row_div.classList.add('small_box');
+        column_div.appendChild(row_div);
     }
-    column_div.style.cssText = 'display: flex; width:400px; height:25px;'
     main_div.appendChild(column_div);
 }
 
-body.appendChild(main_div);
+const boxes = document.querySelectorAll('.small_box');
+boxes.forEach(box => box.addEventListener('mouseover',(e) => {
+    e.target.style.cssText = 'background-color: black;'
+}));
